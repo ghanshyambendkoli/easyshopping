23cba3471b96bb7bdac9fbb8f0d70b5ba3205197
ghanshyambendkoli@gmail.com <ghanshyambendkoli@gmail.com>
2025-08-21 23:02:33 +0530

    smoke test pass case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -19,8 +19,8 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'Increment' button and trigger a frame.
+    await tester.tap(find.text('Increment'));
     await tester.pump();
 
     // Verify that our counter has incremented.
